03908a56381c913b29c5af205a8a14fac6793190
isamohammad <isamohammad@aineurocare.com>
2023-10-17 22:52:34 +0600

    another change

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 
-interface Props {}
 
 const Cart = () => {
   return <div>Cart</div>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 
-interface Props {}
 
 const Home = () => {
   return <div>Home</div>
